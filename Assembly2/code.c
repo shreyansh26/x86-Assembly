30e6a66f391261c4518df589da645cf50b47c593
Shreyansh <shreyansh.pettswood@gmail.com>
2018-08-15 00:07:10 +0530

    Internals of Compiling blog post

---
+++ Assembly2/code.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdio.h>    
+
+#define NUMBER 100
+
+int a = 10;              //Global variable 'a' initialized to 10.
+int b;                   //Global variable uninitialized.
+
+int main() {
+    int c = 123; int number = NUMBER;
+    char d = 'x';
+
+    printf("Hello world!\n");
+    return 0;
+}
+
